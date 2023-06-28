5fbcf7007484436c11e6262c99cfd209d26fa44d
Gabriel <102531193+Zendroo@users.noreply.github.com>
2023-06-28 08:45:32 -0300

    Update attv02-html.js

--- attv02.js
+++ attv02.js
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_adventure.css">
+    <title>Site Adventure Time</title>
+</head>
 
+<body>
+
+<header> 
+<div id="terra">
+<h1>Terra de Ooo</h1>
+</div>
+</header>
+
+<!--Finn></Finn-->
+<div class="finnbox">
+<h2 id="finn">Finn, The Human</h2>
+<p id="texto_finn"> Finn é um garoto humano de 17 anos, que junto com seu <br> 
+                    irmão Jake, vive procurando aventuras pela Terra de Ooo, 
+                    salvando as pessoas de perigos e seres do mal.</p>
+</div>
+<span class="img_finn">
+<img src="https://pm1.aminoapps.com/6796/362f527810b1342c5a12cad5d912b786f8b20ae4v2_hq.jpg">
+</span>
+
+</body>
+</html>
